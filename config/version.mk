43de3d1004418f3b09b38fbb992e299468b58d76
gutssnv <decryptor69@gmail.com>
2026-05-22 18:06:50 +0000

    configs: add support for updater   sync with this commit 1f91fad477de53ae472eddc1bc2059ada9bde51a

--- config/version.mk
+++ config/version.mk
@@ -19,6 +19,7 @@ ifeq ($(TARGET_INCLUDE_UPDATER),true)
 PRODUCT_PRODUCT_PROPERTIES += \
     net.pixelos.build_type=ci \
     net.pixelos.version=$(CUSTOM_VERSION_PROP)
+endif
 
 # Anim Override
 PRODUCT_PRODUCT_PROPERTIES += \
